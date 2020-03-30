9ce30a3876c78f3ac44d0db74bd653d1575e5899
Dmitriy <dmitriy4biz@gmail.com>
2020-03-30 09:28:38 -0300

    Added dedicated network for Docker environment

--- docker/start.sh
+++ docker/start.sh
@@ -2,5 +2,6 @@
 SCRIPT_DIR=$(readlink -f $(dirname "$0"))
 ROOT_DIR=$(dirname ${SCRIPT_DIR})
 
-docker build -t springlearn-fe-image -f ${SCRIPT_DIR}/Dockerfile ${ROOT_DIR}
-docker run --rm --name springlearn-fe-container -d -p 80:80 springlearn-fe-image
+docker build -t springlearn-fe-image -f ${SCRIPT_DIR}/Dockerfile ${ROOT_DIR} || exit 1
+docker network create springlearn-net
+docker run --rm -d --name springlearn-fe --network springlearn-net -p 80:80 springlearn-fe-image
